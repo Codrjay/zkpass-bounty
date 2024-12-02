5afef6e63e7b6ca71471ee0b306bfe8d31433d3f
James Emmanuel <cyanjay69@gmail.com>
2024-12-02 17:53:47 +0000

    Updates

---
+++ precache-manifest.4556fffd338fedfccebcfedc67179c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30b0592ff6c9a8c55380f9463d2e4fe",
+    "url": "/zkpass-bounty/index.html"
+  },
+  {
+    "revision": "a163ee7bd883c264c46b",
+    "url": "/zkpass-bounty/static/css/main.ea6681dc.chunk.css"
+  },
+  {
+    "revision": "3b4b0329a9943a097e5d",
+    "url": "/zkpass-bounty/static/js/2.9afd17d8.chunk.js"
+  },
+  {
+    "revision": "a163ee7bd883c264c46b",
+    "url": "/zkpass-bounty/static/js/main.46b7047c.chunk.js"
+  },
+  {
+    "revision": "7542cce84a6bc7f1bfaf",
+    "url": "/zkpass-bounty/static/js/runtime~main.cf1895f6.js"
+  }
+]);
